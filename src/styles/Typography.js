3c561e2234e65c97d2fda89fbfb813f6ee8190fb
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 12:51:10 +0530

    fonts added

--- src/styles/Typography.js
+++ src/styles/Typography.js
@@ -3,10 +3,9 @@ import MontserratBold from "../assets/fonts/Montserrat-Bold.ttf";
 import MontserratMedium from "../assets/fonts/Montserrat-Medium.ttf";
 import MontserratRegular from "../assets/fonts/Montserrat-Regular.ttf";
 import MontserratSemiBold from "../assets/fonts/Montserrat-SemiBold.ttf";
-import PoppinsBold from "../assets/fonts/Poppins-Bold.ttf";
-import PoppinsMedium from "../assets/fonts/Poppins-Medium.ttf";
-import PoppinsRegular from "../assets/fonts/Poppins-Regular.ttf";
-import PoppinsSemiBold from "../assets/fonts/Poppins-SemiBold.ttf";
+import RobotoBold from "../assets/fonts/Roboto-Bold.ttf";
+import RobotoMedium from "../assets/fonts/Roboto-Medium.ttf";
+import RobotoRegular from "../assets/fonts/Roboto-Regular.ttf";
 
 const Typography = createGlobalStyle`
   @font-face {
@@ -31,22 +30,17 @@ const Typography = createGlobalStyle`
   }
   @font-face {
     font-family: 'PB';
-    src: url(${PoppinsBold});
-    font-style: normal;
-  }
-  @font-face {
-    font-family: 'PSB';
-    src: url(${PoppinsSemiBold});
+    src: url(${RobotoBold});
     font-style: normal;
   }
   @font-face {
     font-family: 'PM';
-    src: url(${PoppinsMedium});
+    src: url(${RobotoMedium});
     font-style: normal;
   }
   @font-face {
     font-family: 'PR';
-    src: url(${PoppinsRegular});
+    src: url(${RobotoRegular});
     font-style: normal;
   }
 `;
