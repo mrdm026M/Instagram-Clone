edd0a05b014e0fe90a0eef380928619f26226ee5
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-06-29 12:51:41 +0530

    profile data added

---
+++ src/assets/data/ProfileData.js
@@ -0,0 +1,24 @@
+const profile = [
+  {
+    image: "",
+    imageAlt: "profile-image",
+    name: "Dhruv Maheshwari",
+    username: "mrdm026",
+    posts: {
+      number: 60,
+      text: "Posts",
+    },
+    followers: {
+      number: 250,
+      text: "Followers",
+    },
+    following: {
+      number: 400,
+      text: "Following",
+    },
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et voluptatibus voluptatem velit quos maiores obcaecati, minima molestias cumque rerum hic.",
+  },
+];
+
+export default profile;
