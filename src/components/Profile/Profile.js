436d1119c87e3f2c2ae62f2c0711ecece90e748a
Dhruv Maheshwari <63752163+mrdm026M@users.noreply.github.com>
2021-07-12 16:28:23 +0530

    profile section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { FiPlus } from "react-icons/fi";
+import { GrUpdate } from "react-icons/gr";
+import { HiOutlineLogout } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
 import ProfileData from "../../assets/data/ProfileData.js";
+import Stories from "../../assets/data/StoryData";
 import "./Profile.scss";
 
 export const Profile = () => {
@@ -19,10 +23,12 @@ export const Profile = () => {
                 <h2>{data.posts.number}</h2>
                 <p>{data.posts.text}</p>
               </div>
+              <hr />
               <div className="user-followers box">
                 <h2>{data.followers.number}</h2>
                 <p>{data.followers.text}</p>
               </div>
+              <hr />
               <div className="user-following box">
                 <h2>{data.following.number}</h2>
                 <p>{data.following.text}</p>
@@ -34,20 +40,35 @@ export const Profile = () => {
             </div>
             <div className="user-story-highlights">
               <h2>Story Highlights</h2>
-              <div className="highlights">
-                <h2>Highlights</h2>
+              <div className="content">
+                {Stories.map((story, index) => {
+                  return (
+                    <div className={index + " story"}>
+                      <div className="profile__img-box">
+                        <img src={story.image} alt="story" />
+                      </div>
+                      <h2>{story.name}</h2>
+                    </div>
+                  );
+                })}
               </div>
             </div>
             <div className="user-btn">
-              <button>
-                <NavLink to="/">Create Post</NavLink>
-              </button>
-              <button>
-                <NavLink to="/">Update Profile</NavLink>
-              </button>
-              <button>
-                <NavLink to="/">Logout</NavLink>
-              </button>
+              <NavLink to="/">
+                <button>
+                  Create Post <FiPlus />
+                </button>
+              </NavLink>
+              <NavLink to="/">
+                <button>
+                  Update Profile <GrUpdate />
+                </button>
+              </NavLink>
+              <NavLink to="/">
+                <button>
+                  Logout <HiOutlineLogout />
+                </button>
+              </NavLink>
             </div>
           </div>
         );
